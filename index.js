a9804fc60b0b18c3a69b6776dd5396669453ea98
Chris Lin <35543509+cslin82@users.noreply.github.com>
2018-02-22 00:12:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
-// add adds two arguments
+// add(a, b) adds two numbers and returns the result:
 
-// 
+function add(a, b) {
+  return a + b;
+}
+
+// subtract(a, b) subtracts b from a and returns the result:
+
+
+function subtract(a, b) {
+  return a - b;
+}
+
+// multiply(a, b) multiplies two numbers and returns the result:
+
+function multiply(a, b) {
+  return a * b;
+}
+
+// divide(a, b) divides a by b and returns the result:
+
+function divide(a, b) {
+  return a/b;
+}
+
+// inc(n) increments n and returns the result:
+
+// dec(n) decrements n and returns the result:
+
+// makeInt(n) parses n as an integer and returns the parsed integer:
+
+// makeInt(n) assumes base 10:
+
+//  makeInt(n) returns NaN as appropriate:
+
+// 10) preserveDecimal(n) preserves n's decimals (it parses n as a floating point number) and returns the parsed number:
+
+// preserveDecimal(n) returns NaN as appropriate:
